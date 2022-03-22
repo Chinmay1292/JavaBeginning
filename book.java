9f33ebbee7700988fedce5e5af12645e98625195
Chinmay1292 <chinmaychougule1212@gmail.com>
2022-03-22 23:20:10 +0530

    Book code

--- book.java
+++ book.java
@@ -1,105 +1,109 @@
-class book
+import java.util.Scanner; public class Book
 {
-    // The fields.
-    private String author;
-    private String title;
-    public int pages;
-    public String refNumber;
-    int borrowed;
-    boolean courseText;
 
-    /**
-     * Set the author and title fields when this object
-     * is constructed.
-     */
-    public book(String bookAuthor, String bookTitle, int bookPages)
-    {
-        author = bookAuthor;
-        title = bookTitle;
-        pages = bookPages;
-        refNumber = "";
-    }
+Scanner scan = new Scanner(System.in); 
+private String author;
+private String title; 
+private int pages;
+private String refNumber;
+ 
+private int borrowed; 
+private boolean courseText;
+public Book(String bookAuthor, String bookTitle, int bookPages, boolean bookCourseText)
 
-    //Exercise 1
-    public String getAuthor()
-    {
-        return author;
-    }
-    
-    public String getTitle()
-    {
-        return title;
-    }
+{
 
-    //Exercise 3
-    public int getPages()
-    {
-        return pages;
-    }
+author = bookAuthor; title = bookTitle; pages = bookPages; refNumber = ""; borrowed = 0;
+courseText = bookCourseText;
 
-    //Exercise 6
-    public String getRefNumber()
-    {
-        return refNumber;
-    }
-
-    //Exercise 7
-    public int getBorrowed()
-    {
-        return borrowed;
-    }
-
-    public void setRefNumber(String ref)
-    {
-        //Exercise 8
-        if (ref.length()>=3)
-        {
-            refNumber = ref;
-        }
-        else
-        {
-            System.out.println("Refernce # must be at least 3 characters");
-        }
-    }
-
-    //Exercise 9
-    public void borrow()
-    {
-        borrowed = borrowed +1;
-    }
-
-    //Exercise 10
-    public boolean isCourseText()
-    {
-        return courseText;
-    }
-
-    //Exercise 2
-    public void printAuthor()
-    {
-        System.out.println("Author is: " + author);
-    }
-    
-    public void printTitle()
-    {
-        System.out.println("Title is: " + title);
-    }
-
-    //Exercise 5
-    public void printDetails()
-    {
-    System.out.println("Title is: " + title + " and Author is: " + author +" and it has pages: "+ pages);
-    
-
-    //Exercise 7
-    if(refNumber.length()>0)
-    {
-        System.out.println("Reference number: " + refNumber);
-    }
-    else
-    {
-        System.out.println("ZZZ");
-    }
-System.out.println("\" " + title + "\" has been borrowed " + borrowed + " times");
-    }
 }
+
+public String getAuthor()
+
+{
+
+return author;
+
+}
+public String getTitle()
+
+{
+
+return title;
+
+}
+public String getRefNumber()
+
+{
+
+return refNumber;
+
+}
+public int getPages()
+ 
+{
+
+return pages;
+
+}
+public int getBorrowed()
+
+{
+
+return borrowed;
+
+}
+public boolean isCourseText()
+
+{
+
+return courseText;
+
+}
+public void printAuthor()
+
+{
+
+System.out.println(author);
+
+}
+public void printTitle()
+
+{
+
+System.out.println(title);
+
+}
+public void setRefNumber(String ref)
+
+{
+
+if (ref.length() >= 3)
+{
+refNumber = ref; System.out.println("Set success!");
+}
+else 
+System.out.println("Set failed.");
+
+}
+public void borrow()
+
+{
+borrowed = borrowed + 1;
+}
+public void printDetails()
+{
+System.out.println("Title :" + title); 
+System.out.println("Author :" + author); 
+System.out.println("Page(s) :" + pages); 
+if (refNumber.length() != 0)
+{
+System.out.println("Ref. Num :" + refNumber);
+}
+
+else 
+System.out.println("Ref. Num :ZZZ"); 
+System.out.println("Borrowed :" + borrowed);
+}
+} 
+

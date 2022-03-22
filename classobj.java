ac3591a926c3d6a266bb61cc585de785cfe8ad04
Chinmay1292 <chinmaychougule1212@gmail.com>
2022-03-22 15:59:27 +0530

    class and object

--- classobj.java
+++ classobj.java
@@ -1,23 +1,35 @@
 public class classobj {
     String title;
     String author;
-    classobj(String s, String a)
+    int price;
+    classobj(String s, String a, int p)
     {
         this.title = s;
         this.author = a;
+        this.price = p;
     }
     String getTitle()
     {
         return this.title;
     }
+    String getAuthor()
+    {
+        return this.author;
+    }
+    int getPrice()
+    {
+        return this.price;
+    }
     public static void main(String[] args)
     {
         classobj x;
-        x = new classobj("Programming","Java");
-        System.out.println(x.getTitle()); //membership operator
+        x = new classobj("Programming","Man",1000);
+        System.out.println("The title of the book: "+ x.getTitle() + " and its Author is: " + x.getAuthor() +" and its price is: "+ x.getPrice()); //membership operator
 
-        classobj textBook;
-        textBook = x;
-        String titleUppercase = textBook.getTitle().toUpperCase(); // reference variable
+        //classobj textBook;
+        //textBook = x;
+        //String titleUppercase = textBook.getTitle().toUpperCase(); // reference variable
+        //System.out.println(titleUppercase);
+
     }
 }

4aa234a8b6f1463c4d981abba27c8ce4f212aa91
Chinmay1292 <chinmaychougule1212@gmail.com>
2022-03-24 17:40:14 +0530

    public private protected defauly

---
+++ Test.java
@@ -0,0 +1,9 @@
+import packageA.A;
+public class Test {
+    public static void main(String[] args)
+    {
+        A obj = new A();
+        aobj = 
+
+    }
+}
---
+++ packageA/A.java
@@ -0,0 +1,12 @@
+package packageA;
+
+public class A {
+    public static void main(String[] a){
+        System.out.println("Hello");
+    }
+    public void f()
+    {
+        System.out.println("Hey");
+
+    }
+}
